173a0370ff24d6ac44b17af3a229b1f17edacb8d
Juan Antonio Morales <juanan4290@gmail.com>
2017-12-15 18:57:44 +0100

    update introductionToPython.py

--- python/introductionToPython.py
+++ python/introductionToPython.py
@@ -1,5 +1,7 @@
-#Exercise 1
-#Given two int values, return their sum if the two values are not the same. If the two values are not the same then return the double of their sum:
+# Exercise 1
+# Given two int values, return their sum if the two 
+# values are not the same. If the two values are not 
+# the same then return the double of their sum:
 
 def q1(a,b):
     if a!=b:
@@ -8,8 +10,9 @@ def q1(a,b):
         return 2*(a+b)
 
 
-#Exercise 2
-#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10:
+# Exercise 2
+# Given 2 ints, a and b, return True if one if them is 
+# 10 or if their sum is 10:
 
 def q2(a,b):
     if a==10 or b==10 or (a+b)==10:
@@ -18,8 +21,12 @@ def q2(a,b):
         return False
 
 
-#Exercise 3
-#Write a function “centenario” that will take Name, and year of birth as inputs, check if year of birth is int and cast it to int if not, and print name together with the text explaining when the person is to have 100 years:
+# Exercise 3
+# Write a function “centenario” that will take Name, 
+# and year of birth as inputs, check if year of birth 
+# is int and cast it to int if not, and print name 
+# together with the text explaining when the person is 
+# to have 100 years:
 
 def centenario(name,year):
     if type(year)!=int:
